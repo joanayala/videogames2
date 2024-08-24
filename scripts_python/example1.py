7c9f1f56a520bad542e25b74fd40c513b831c7a0
Joan <jflaco85@gmail.com>
2024-08-23 21:01:22 -0500

    Script python 1

---
+++ scripts_python/example1.py
@@ -0,0 +1,40 @@
+#Este es un comentario de línea
+
+'''
+Esto es un comentario
+de bloque
+'''
+
+data = '''
+ Esto es una variable string
+ en bloque
+'''
+
+n1 = 10
+n2 = 20
+suma = n1 + n2
+
+print(suma)
+print("La suma es: ", suma)
+print(f"La suma es: {suma}")
+print("La suma es: ", n1 + n2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
